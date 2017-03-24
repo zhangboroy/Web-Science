3030ff7a3b2876d2ba97975c45793cb72bb09b1e
zhangboroy <bzhan001@odu.edu>
2017-03-23 22:20:52 -0400

    Add files via upload

--- assg06_submission/homophilyTest.py
+++ assg06_submission/homophilyTest.py
@@ -30,13 +30,35 @@ f = open("genderTable.txt", "w", encoding='utf-8')
 f.write('Username')
 for i in range(12):
     f.write(' ')
+f.write('Gender\tUsername')
+for i in range(12):
+    f.write(' ')
+f.write('Gender\tUsername')
+for i in range(12):
+    f.write(' ')
 f.write('Gender\n')
-for element in genderTable:
-    f.write(element[1])
-    for i in range(20-len(element[1])):
+for i in range(0,int(len(genderTable)/3)*3,3):
+    f.write(genderTable[i][1])
+    for j in range(20-len(genderTable[i][1])):
         f.write(' ')
-    f.write(element[0]+'\n')
-
+    f.write(genderTable[i][0]+'\t'+genderTable[i+1][1])
+    for j in range(20-len(genderTable[i+1][1])):
+        f.write(' ')
+    f.write(genderTable[i+1][0]+'\t'+genderTable[i+2][1])
+    for j in range(20-len(genderTable[i+2][1])):
+        f.write(' ')
+    f.write(genderTable[i+2][0]+'\n')
+if i+3<len(genderTable):
+    f.write(genderTable[i+3][1])
+    for j in range(20-len(genderTable[i+3][1])):
+        f.write(' ')
+    f.write(genderTable[i+3][0])
+if i+4<len(genderTable):
+    f.write('\t'+genderTable[i+4][1])
+    for j in range(20-len(genderTable[i+4][1])):
+        f.write(' ')
+    f.write(genderTable[i+4][0])
+f.write('\n')
 f.close()
 
 genderTable100=[]
@@ -51,15 +73,45 @@ f = open("genderTable100.txt", "w", encoding='utf-8')
 f.write('Username')
 for i in range(12):
     f.write(' ')
+f.write('Gender\tUsername')
+for i in range(12):
+    f.write(' ')
+f.write('Gender\tUsername')
+for i in range(12):
+    f.write(' ')
 f.write('Gender\n')
-for element in genderTable100:
-    if element[0]=='female':
+for i in range(0,int(len(genderTable100)/3)*3,3):
+    if genderTable100[i][0]=='female':
         edgesCross = edgesCross + 1
-    f.write(element[1])
-    for i in range(20-len(element[1])):
+    if genderTable100[i+1][0]=='female':
+        edgesCross = edgesCross + 1
+    if genderTable100[i+2][0]=='female':
+        edgesCross = edgesCross + 1
+    f.write(genderTable100[i][1])
+    for j in range(20-len(genderTable100[i][1])):
         f.write(' ')
-    f.write(element[0]+'\n')
-
+    f.write(genderTable100[i][0]+'\t'+genderTable100[i+1][1])
+    for j in range(20-len(genderTable100[i+1][1])):
+        f.write(' ')
+    f.write(genderTable100[i+1][0]+'\t'+genderTable100[i+2][1])
+    for j in range(20-len(genderTable100[i+2][1])):
+        f.write(' ')
+    f.write(genderTable100[i+2][0]+'\n')
+if i+3<len(genderTable100):
+    if genderTable100[i+3][0]=='female':
+        edgesCross = edgesCross + 1
+    f.write(genderTable100[i+3][1])
+    for j in range(20-len(genderTable100[i+3][1])):
+        f.write(' ')
+    f.write(genderTable100[i+3][0])
+if i+4<len(genderTable100):
+    if genderTable100[i+4][0]=='female':
+        edgesCross = edgesCross + 1
+    f.write('\t'+genderTable100[i+4][1])
+    for j in range(20-len(genderTable100[i+4][1])):
+        f.write(' ')
+    f.write(genderTable100[i+4][0])
+f.write('\n')
 f.close()
 
 print ('Males: {}'.format(len(genderTable100)-edgesCross)+'\tFemales: {}'.format(edgesCross))
